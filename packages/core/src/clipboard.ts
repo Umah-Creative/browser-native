e90443790ed381bcdc72b2041850b5804ac9e622
Richie Permana <richie.permana@gmail.com>
2025-11-20 22:36:30 +0800

    feat: added type and better comments

--- packages/core/src/clipboard.ts
+++ packages/core/src/clipboard.ts
@@ -1,10 +1,22 @@
-export async function readClipboardText(): Promise<string> {
+import { ClipboardText } from '@umah-creative/browser-native-types';
+
+/**
+ * Reads the current text from the clipboard.
+ */
+export async function readClipboardText(): ReturnType<
+  ClipboardText['readClipboardText']
+> {
   if (!navigator.clipboard) throw new Error('Clipboard API not supported');
 
   return navigator.clipboard.readText();
 }
 
-export async function writeClipboardText(text: string): Promise<void> {
+/**
+ * Writes the given text to the clipboard.
+ */
+export async function writeClipboardText(
+  text: string
+): ReturnType<ClipboardText['writeClipboardText']> {
   if (!navigator.clipboard) throw new Error('Clipboard API not supported');
 
   return navigator.clipboard.writeText(text);
