c21a8d7f27e9fe4fb27740fe7b902de70c5cf203
Richie Permana <richie.permana@gmail.com>
2025-11-20 22:34:27 +0800

    feat: added loading state and throw error

--- packages/react/src/use-clipboard.ts
+++ packages/react/src/use-clipboard.ts
@@ -3,23 +3,55 @@ import {
   readClipboardText,
   writeClipboardText,
 } from '@umah-creative/browser-native';
+import { UseClipboardResult } from '@umah-creative/browser-native-types';
 
-export function useClipboard() {
+/**
+ * A React hook that provides a simple interface for reading and writing to the clipboard.
+ */
+export function useClipboard(): UseClipboardResult {
   const [text, setText] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
 
   const read = useCallback(async () => {
-    const content = await readClipboardText();
-    setText(content);
-    return content;
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const content = await readClipboardText();
+      setText(content);
+
+      return content;
+    } catch (err) {
+      const errorObj = err instanceof Error ? err : new Error(String(err));
+      setError(errorObj);
+
+      throw errorObj;
+    } finally {
+      setIsLoading(false);
+    }
   }, []);
 
   const write = useCallback(async (newText: string) => {
-    await writeClipboardText(newText);
-    setText(newText);
+    setIsLoading(true);
+    setError(null);
+    try {
+      await writeClipboardText(newText);
+      setText(newText);
+    } catch (err) {
+      const errorObj = err instanceof Error ? err : new Error(String(err));
+      setError(errorObj);
+
+      throw errorObj;
+    } finally {
+      setIsLoading(false);
+    }
   }, []);
 
   return {
     text,
+    isLoading,
+    error,
 
     read,
     write,
